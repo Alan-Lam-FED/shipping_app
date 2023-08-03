f668ef7f981af2a7913ba5b85670ccfd8ecd2792
Lam Quoc Phu <d.fed26@gsoft01.tk>
2023-08-03 09:20:38 +0700

    create foto feature

---
+++ App.js
@@ -0,0 +1,22 @@
+import { registerRootComponent } from "expo";
+import React, { useMemo, useState } from "react";
+import "react-native-gesture-handler";
+import { View, Text } from "react-native";
+import CameraPage from "./src/pages/camera/camera";
+import Home from "./src/pages/home/home";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+function App() {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Camera" component={CameraPage}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+registerRootComponent(App);
